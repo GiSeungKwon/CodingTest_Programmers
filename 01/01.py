658601b1f0465c77c539d0f043fc3be21766c42b
GiSeungKwon <rltmd1994@gmail.com>
2026-01-07 13:34:20 +0900

    second

--- 01/01.py
+++ 01/01.py
@@ -1,15 +1,14 @@
 str = input()
-print(f"type(str): {type(str)} str: {str}")
-
 list_str = list(str.replace("[","").replace("]","").split(','))
-print(f"type(list_str): {type(list_str)} list_str: {list_str}")
 
 def solution(param_list_str):
-    print(f"type(param_list_str): {type(param_list_str)} list_str: {param_list_str}")
-    if not(2 <= len(param_list_str) <= 10 ** 5):
-        return "Exception"
+    return sorted(param_list_str)
+
+def solution2(param_list_str):
     param_list_str.sort()
-    # print(f"type(param_list_str): {type(param_list_str)} list_str: {param_list_str}")
     return param_list_str
+
 print(f"solution(list_str): {solution(list_str)}")
+print(f"list_str: {list_str}")
+print(f"solution2(list_str): {solution2(list_str)}")
 print(f"list_str: {list_str}")
