fe2a3e6df2a809dc6636753c0c139967873abce9
GiSeungKwon <rltmd1994@gmail.com>
2026-01-14 16:26:17 +0900

    -

--- 04/04.py
+++ 04/04.py
@@ -4,20 +4,18 @@ def to_list(param_text):
     return ast.literal_eval(param_text)
 
 def make_pattern(param_list):
-    answer_len = len(param_list)
-
     person1 = [1, 2, 3, 4, 5]
     person2 = [2, 1, 2, 3, 2, 4, 2, 5]
     person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    person1 = person1 * ((answer_len // len(person1)) + 1)
-    person1 = person1[:answer_len]
+    person1 = person1 * ((len(param_list) // len(person1)) + 1)
+    person1 = person1[:len(param_list)]
 
-    person2 = person2 * ((answer_len // len(person2)) + 1)
-    person2 = person2[:answer_len]
+    person2 = person2 * ((len(param_list) // len(person2)) + 1)
+    person2 = person2[:len(param_list)]
 
-    person3 = person3 * ((answer_len // len(person3)) + 1)
-    person3 = person3[:answer_len]
+    person3 = person3 * ((len(param_list) // len(person3)) + 1)
+    person3 = person3[:len(param_list)]
     return param_list, person1, person2, person3
 
 def count_correct(answer, person1, person2, person3):
