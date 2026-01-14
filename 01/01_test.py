78f796f24b299066d546784b131e49ae79f3b947
GiSeungKwon <rltmd1994@gmail.com>
2026-01-14 16:41:30 +0900

    0107_17:36

--- 01/01_test.py
+++ 01/01_test.py
@@ -1,5 +1,9 @@
+# [1,-5,2,4,3] [-5,1,2,4,3]
+# [2,1,1,3,2,5,4] [1,1,2,2,3,4,5]
+# [6,1,7] [1,6,7]
 def solution(param_list):
-    param_list.sort()
-    return param_list
+    return sorted(param_list)
 
 print(solution([1,-5,2,4,3]))
+print(solution([2,1,1,3,2,5,4]))
+print(solution([6,1,7]))
