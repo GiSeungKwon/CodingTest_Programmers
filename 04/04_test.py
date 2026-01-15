8946d5a300c0e67469f0b97b520108757faa3488
GiSeungKwon <rltmd1994@gmail.com>
2026-01-15 13:37:02 +0900

    0115_13:36

--- 04/04_test.py
+++ 04/04_test.py
@@ -1,26 +1,24 @@
-# [1,2,3,4,5] [1]
-# [1,3,2,4,2] [1,2,3]
+case1 = [1,2,3,4,5] # [1]
+case2 = [1,3,2,4,2] # [1,2,3]
+
 def solution(param_list):
-    patterns = [[1,2,3,4,5],
-                [2,1,2,3,2,4,2,5],
-                [3,3,1,1,2,2,4,4,5,5]]
-    result = [0 for _ in range(len(patterns))]
-
-    # i = 정답 index
+    patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    count_correct = [0 for _ in range(len(patterns))]
     for i in range(len(param_list)):
-        # j = patterns index
         for j in range(len(patterns)):
-            if param_list[i] == patterns[j][i%len(patterns[j])]:
-                result[j] += 1
-    result_idx = []
-    max_value = max(result)
-    for i in range(len(result)):
-        if result[i] == max_value:
-            result = tuple(result)
-            result_idx.append(result.index(result[i]) + 1)
-            result = list(result)
-            result[i] = -1
-    return result_idx
+            idx = i % len(patterns[j])
+            if param_list[i] == patterns[j][idx]:
+                count_correct[j] += 1
+    result = []
+    max_value = max(count_correct)
+    for i in range(len(patterns)):
+        if max_value == count_correct[i]:
+            result.append(i+1)
+    return result
 
-# print(solution([1,2,3,4,5]))
-print(solution([1,3,2,4,2]))
+print(case1, "->", solution(case1))
+print(case2, "->", solution(case2))
