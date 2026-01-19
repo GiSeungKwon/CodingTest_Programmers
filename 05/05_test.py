e5598786629241cfaebf6c8de366823dff8cd5c4
GiSeungKwon <rltmd1994@gmail.com>
2026-01-19 19:52:21 +0900

    0119_19:52

--- 05/05_test.py
+++ 05/05_test.py
@@ -1,15 +1,6 @@
 case1_1, case1_2 = [[1,4],[3,2],[4,1]], [[3,3],[3,3]] # [[15,15],[15,15],[15,15]]
 case2_1, case2_2 = [[2,3,2],[4,2,4],[3,1,4]], [[5,4,3],[2,4,1],[3,1,1]] # [[22,22,11],[36,28,18],[29,20,14]]
 
-def solution(arr1, arr2):
-    r1, c1 = len(arr1), len(arr1[0])
-    r2, c2 = len(arr2), len(arr2[0])
-    result = [[0 for _ in range(c2)] for _ in range(r1)]
-    for i in range(r1):
-        for j in range(c2):
-            for k in range(c1):
-                result[i][j] += arr1[i][k] * arr2[k][j]
-    return result
 
 
 print(solution(case1_1, case1_2))
--- 06/06_0_test.py
+++ 06/06_0_test.py
@@ -1,18 +1,7 @@
 case1_1, case1_2 = 5, [2,1,2,6,2,4,3,3] # [3,4,2,1,5]
 case2_1, case2_2 = 4, [4,4,4,4,4] # [4,1,2,3]
 
-def solution(n, stages):
-    challengers = [0 for _ in range(n+2)] # n: 5 challengers: [0, 1, 2, 3, ..., 6]
-    for item in stages:
-        challengers[item] += 1
-    print(f"challengers: {challengers}")
-    fails = {}
-    total = len(stages)
-    for i in range(1, n+1):
-        fails[i] = challengers[i] / total
-        total -= challengers[i]
-    print(f"fails: {fails}")
-    return sorted(fails, key=lambda x:fails[x], reverse=True)
+
 
 print(solution(case1_1, case1_2))
 print()
