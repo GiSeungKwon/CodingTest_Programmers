02ef14b1fc063b61bfd6ad7277c96ed5de25ecdd
GiSeungKwon <rltmd1994@gmail.com>
2026-01-22 15:46:11 +0900

    0122_1546

--- 07/07_test.py
+++ 07/07_test.py
@@ -1,7 +1,20 @@
 case1 = "ULURRDLLU" # 7
 case2 = "LULLLLLLU" # 7
 
+def solution(str):
+    d = {"U":(0,1), "D":(0,-1), "R":(1,0), "L":(-1,0)}
+    x, y = 0, 0
+    path = set()
 
+    for char in str:
+        print(char, end="->")
+        nx, ny = x+d[char][0], y+d[char][1]
+        print(f"x:{x} y:{y} nx:{nx} ny:{ny}")
+        if -5<=nx<=5 and -5<=ny<=5:
+            path.add((x,y,nx,ny))
+            path.add((nx,ny,x,y))
+            x, y = nx, ny
+    return len(path) // 2
 
 print(case1, "->", solution(case1))
 print(case2, "->", solution(case2))
