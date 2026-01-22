685e1857c0f47f8f90b406f68808b4f49917a998
GiSeungKwon <rltmd1994@gmail.com>
2026-01-22 15:58:12 +0900

    0122_15:58

--- 08/08_test.py
+++ 08/08_test.py
@@ -2,7 +2,19 @@ case1 = "(())()" # 7
 case2 = "((())()" # 7
 case3 = ")()()()"
 
-
+def solution(str):
+    stack = []
+    for char in str:
+        if char == "(":
+            stack.append("(")
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    if not stack:
+        return True
+    else:
+        return False
 
 print(case1, "->", solution(case1))
 print(case2, "->", solution(case2))
